b69e1c4bf81b3faed9f9d17e369bf4ae95d949a1
shilpi <gargshilpi22@gmail.com>
2012-11-20 20:23:23 -0600

    boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofmysize::Application.config.secret_token = 'ad088f0b921fca86e86c915d97ad89baab2aa6e0dd6878b3f6219dad302643770c02c25bec1c73c630e52e8f9ff8e31a61d855a485ebc55db90251c939c2c9c9'
